fcbbf21b4babb4f1fb35d5eef9cc70a04e93a0b2
Mikkel Wienberg Madsen <mikkel.wienberg@gmail.com>
2022-08-06 15:19:59 +0200

    KubeJS create

---
+++ kubejs/startup_scripts/create-delight.js
@@ -0,0 +1,206 @@
+onEvent('recipes', event => {
+    event.recipes.createMixing('farmersdelight:hot_cocoa', [
+        'minecraft:glass_bottle',
+        'minecraft:sugar',
+        '2x minecraft:cocoa_beans',
+        'minecraft:milk'
+    ]).heated()
+
+    event.recipes.createMixing('farmersdelight:apple_cider', [
+        'minecraft:glass_bottle',
+        'minecraft:sugar',
+        '2x minecraft:apple',
+    ]).heated()
+
+    event.recipes.createMixing('2x farmersdelight:dumplings', [
+        'farmersdelight:raw_pasta',
+        'farmersdelight:raw_pasta',
+        '#forge:salad_ingredients',
+        'farmersdelight:onion',
+        '#createlive:dumplings'
+    ]).heated()
+
+    event.recipes.createMixing('farmersdelight:cooked_rice',[
+        'minecraft:bowl',
+        'farmersdelight:rice'
+    ]).heated()
+
+
+    event.recipes.createMixing('farmersdelight:beef_stew',[
+        'minecraft:bowl',
+        '#forge:raw_beef',
+        'minecraft:carrot',
+        'minecraft:potato'
+    ]).heated()
+
+    event.recipes.createMixing('farmersdelight:chicken_soup', [
+        'minecraft:bowl',
+        '#forge:raw_chicken',
+        'minecraft:carrot',
+        '#forge:salad_ingredients',
+        '#forge:vegetables'
+    ]).heated()
+
+    event.recipes.createMixing('farmersdelight:fish_stew', [
+        'minecraft:bowl',
+        'minecraft:carrot',
+        'minecraft:potato',
+        'minecraft:beetroot',
+        '#forge:salad_ingredients'
+    ]).heated()
+
+    event.recipes.createMixing('farmersdelight:fried_rice', [
+        'minecraft:bowl',
+        'farmersdelight:rice',
+        '#forge:eggs',
+        'minecraft:carrot',
+        'farmersdelight:onion'
+    ]).heated()
+
+
+    event.recipes.createMixing('farmersdelight:pumpkin_soup', [
+        'minecraft:bowl',
+        'farmersdelight:pumpkin_slice',
+        'forge:salad_ingredients',
+        'forge:raw_pork'
+    ]).heated()
+
+
+    event.recipes.createMixing('farmersdelight:baked_cod_stew', [
+        'minecraft:bowl',
+        'tags:forge:raw_fishes/cod',
+        'minecraft:potato',
+        'forge:eggs',
+        'farmersdelight:tomato'
+    ]).heated()
+
+
+    event.recipes.createMixing('farmersdelight:noodle_soup', [
+        'minecraft:bowl',
+        'farmersdelight:raw_pasta',
+        'farmersdelight:fried_egg',
+        'minecraft:dried_kelp',
+        '#tags:forge:raw_pork'
+    ]).heated()
+
+
+    event.recipes.createMixing('farmersdelight:pasta_with_meatballs', [
+        'minecraft:bowl',
+        'farmersdelight:minced_beef',
+        'farmersdelight:raw_pasta',
+        'farmersdelight:tomato_sauce'
+    ]).heated()
+
+
+    event.recipes.createMixing('farmersdelight:pasta_with_mutton_chop', [
+        'minecraft:bowl',
+        '#forge:raw_mutton',
+        'farmersdelight:raw_pasta',
+        'farmersdelight:tomato_sauce'
+    ]).heated()
+
+    
+    event.recipes.createMixing('farmersdelight:vegetable_noodles', [
+        'minecraft:bowl',
+        'minecraft:carrot',
+        'minecraft:brown_mushroom',
+        'farmersdelight:raw_pasta',
+        '#forge:salad_ingredients',
+        '#forge:vegetables'
+    ]).heated()
+
+
+    event.recipes.createMixing('farmersdelight:ratatouille', [
+        'minecraft:bowl',
+        'farmersdelight:tomato',
+        'farmersdelight:onion',
+        'minecraft:beetroot',
+        '#forge:vegetables'
+    ]).heated()
+
+
+    event.recipes.createMixing('farmersdelight:squid_ink_pasta', [
+        'minecraft:bowl',
+        '#forge:raw_fishes',
+        'farmersdelight:raw_pasta',
+        'farmersdelight:tomato',
+        'minecraft:ink_sac'
+    ]).heated()
+
+    
+    event.recipes.createMixing('farmersdelight:stuffed_pumpkin_block', [
+        'minecraft:pumpkin',
+        'farmersdelight:rice',
+        '#forge:vegetables',
+        'farmersdelight:tomato',
+        'minecraft:baked_potato',
+        'farmersdelight:brown_mushroom_colony',
+        'minecraft:sweet_berries'
+    ]).heated()
+
+
+
+    event.recipes.createMixing('farmersdelight:dog_food', [
+        'minecraft:bowl',
+        'minecraft:rotten_flesh',
+        'minecraft:bone_meal',
+        '#farmersdelight:wolf_prey',
+        'farmersdelight:rice'
+    ]).heated()
+
+    event.recipes.createCutting(
+        '4x farmersdelight:pumpkin_slice',
+        'minecraft:pumpkin'
+    ).processingTime(50)
+
+    event.recipes.createCutting(
+        '2x farmersdelight:minced_beef',
+        'minecraft:beef'
+    ).processingTime(50)
+
+    event.recipes.createCutting(
+        ['2x farmersdelight:chicken_cuts', 'minecraft:bone_meal'],
+        'minecraft:chicken'
+    ).processingTime(50)
+
+    event.recipes.createCutting(
+        '2x farmersdelight:bacon'
+        'minecraft:porkchop'
+    ).processingTime(50)
+
+    event.recipes.createCutting(
+        ['2x farmersdelight:cod_slice', 'minecraft:bone_meal'],
+        'minecraft:cod'
+    ).processingTime(50)
+
+    event.recipes.createCutting(
+        ['2x farmersdelight:salmon_slice', 'minecraft:bone_meal'],
+        'minecraft:salmon'
+    ).processingTime(50)
+
+    event.recipes.createCutting(
+        '2x farmersdelight:mutton_chops',
+        'minecraft:mutton'
+    ).processingTime(50)
+
+    event.recipes.createCutting(
+        '7x farmersdelight:cake_slice',
+        'minecraft:cake'
+    ).processingTime(50)
+
+    event.recipes.createCutting(
+        '4x farmersdelight:apple_pie_slice',
+        'farmersdelight:apple_pie'
+    ).processingTime(50)
+
+    event.recipes.createCutting(
+        '4x farmersdelight:sweet_berry_cheesecake_slice',
+        'farmersdelight:sweet_berry_cheesecake'
+    ).processingTime(50)
+
+    event.recipes.createCutting(
+        '4x farmersdelight:chocolate_pie_slice',
+        'farmersdelight:chocolate_pie'
+    ).processingTime(50)
+
+})
